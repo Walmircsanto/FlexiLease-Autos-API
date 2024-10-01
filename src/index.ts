e1abd6a55788d04e2fbf3ef569dfb8107bf5c2bf
Walmircsanto <walmirsantosrosa@gmail.com>
2024-10-01 16:51:46 -0300

    :sparkles: feat: create application main file

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import bodyParser from "body-parser";
+import {AppDataSource} from './api/shared/typeorm/connection'
+//import routes from "./api/shared/http/routes";
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+//app.use('/api', routes); // estou indicando que meu servidor vai usar essas rotas
+
+//inicializar o banco de dados
+AppDataSource.initialize().then(r => {
+    console.log("data source initialized in port");
+}).catch((err) =>{
+    console.log(err);
+});
+
+app.listen(3030,()=>{
+    console.log("Server is running on port 3000");
+})
