416ced6b3c36eaa8e7b58d4be030d144eda29bfc
Walmircsanto <walmirsantosrosa@gmail.com>
2024-10-05 16:30:01 -0300

    :sparkles: feat: add method in controller to create session

---
+++ src/api/modules/user/controller/UserController.ts
@@ -0,0 +1,19 @@
+import {Request, Response} from 'express';
+import UserService from "../service/UserService";
+
+export default class UserController {
+
+    public async createUser(req: Request, res: Response) {
+        const {name, cpf, birth, cep, email, password} = req.body
+
+        const userService = new UserService();
+
+        const user = await userService.createUser({name, cpf, birth, cep, email, password})
+        return res.status(200).json(user);
+    }
+
+    public testeStringToken(req: Request, res: Response) {
+
+        return res.json("Deu certoo")
+    }
+}
