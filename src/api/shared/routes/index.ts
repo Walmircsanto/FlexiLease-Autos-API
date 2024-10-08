5dbc6d4f848920c601d9c1d314b957bcf5a818d8
Walmircsanto <walmirsantosrosa@gmail.com>
2024-10-08 16:45:24 -0300

    :sparkles: feat: add backup route in main route file

--- src/api/shared/routes/index.ts
+++ src/api/shared/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import userRoutes from "../../modules/user/routes/User.routes";
 import sessionRoutes from "../../modules/user/routes/Session.routes";
 import carRoutes from "../../modules/cars/routes/CarRoutes";
+import reserveRoutes from "../../modules/reserve/routes/reserveRoutes";
 
 const routes = Router();
 
 routes.use("/user", userRoutes);
 routes.use("/session", sessionRoutes)
 routes.use("/car", carRoutes)
+routes.use("/reserve", reserveRoutes)
 
 export default routes
