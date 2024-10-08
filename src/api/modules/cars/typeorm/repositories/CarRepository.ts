001096b7925bb4c398f244366af2dba174748812
Walmircsanto <walmirsantosrosa@gmail.com>
2024-10-08 16:40:01 -0300

    :sparkles: feat(CarRepository): add possible return of null in findByIdCar

--- src/api/modules/cars/typeorm/repositories/CarRepository.ts
+++ src/api/modules/cars/typeorm/repositories/CarRepository.ts
@@ -5,7 +5,7 @@ import {Entity, Repository} from "typeorm";
 
 
 export const CarRepository = AppDataSource.getRepository(Car).extend({
-    async finById(id: number) {
+    async finById(id: number):Promise<Car | null> {
         const car = await this.findOne({
             where: {id: id}
         });
