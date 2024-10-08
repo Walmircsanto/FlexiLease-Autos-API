694e1f8a9547ad5e1c7a2a1b5822c691744e98f2
Walmircsanto <walmirsantosrosa@gmail.com>
2024-10-08 05:27:31 -0300

    :sparkles: feat(CarService) create car service

---
+++ src/api/modules/cars/service/CarService.ts
@@ -0,0 +1,49 @@
+import {AppDataSource} from "@modules/typeorm/connection";
+import Car from "../typeorm/entities/Car";
+
+import {Repository} from "typeorm";
+import IRequestCar from "../DTO/RequestCarDTO";
+import {CarRepository} from "../typeorm/repositories/CarRepository";
+import AppError from "@modules/errors/AppError";
+
+class CarService   {
+    private ormRepository: Repository<Car>;
+
+
+    constructor() {
+        this.ormRepository = AppDataSource.getRepository(Car);
+    }
+
+    async findById(id: number): Promise<Car | null> {
+        const carRepository =  CarRepository
+
+        return await carRepository.finById(id);
+    }
+
+    async createCar({ model, color, year, valuePerDay, acessories = [], numberOfPassengers }: IRequestCar): Promise<Car> {
+        const carRepository =  CarRepository
+
+        if(year < 1950 || year > 2023 ){
+            throw new AppError("The year of the car is not valid", "Bad Request", 400);
+        }
+        const car = carRepository.create({
+            model,
+            color,
+            year,
+            valuePerDay,
+            acessories,
+            numberOfPassengers
+        });
+
+        console.log(acessories)
+
+        await this.ormRepository.save(car);
+        return car;
+    }
+
+    public async finAllCars(){
+        return await CarRepository.find();
+    }
+}
+
+export default CarService;
