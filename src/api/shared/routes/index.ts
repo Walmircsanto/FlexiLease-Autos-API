49a137ebc61d8f0cf56f6313b386f1972a2aedfb
Walmircsanto <walmirsantosrosa@gmail.com>
2024-10-08 05:41:42 -0300

    :sparkles: feat(index.ts) add routes from to car feature

--- src/api/shared/routes/index.ts
+++ src/api/shared/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import userRoutes from "../../modules/user/routes/User.routes";
 import sessionRoutes from "../../modules/user/routes/Session.routes";
+import carRoutes from "../../modules/cars/routes/CarRoutes";
 
 const routes = Router();
 
 routes.use("/user", userRoutes);
 routes.use("/session", sessionRoutes)
+routes.use("/car", carRoutes)
 
 export default routes
