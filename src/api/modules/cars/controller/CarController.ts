4e51163ba6cc5d02a206a91cb9a9911ad9b6e998
Walmircsanto <walmirsantosrosa@gmail.com>
2024-10-08 16:32:57 -0300

    :sparkles: feat: create add car delete method to controller

--- src/api/modules/cars/controller/CarController.ts
+++ src/api/modules/cars/controller/CarController.ts
@@ -1,31 +1,34 @@
 import {Request, Response} from 'express';
 import CarService from "../service/CarService";
 import car from "../typeorm/entities/Car";
+import ICarService from "../typeorm/repositories/ICarService";
+import {inject, injectable} from "tsyringe";
+
 
 export default class CarController {
 
 
-    public async createrCar(req: Request, res: Response): Promise<Response> {
-        const {model, color, year, valuePerDay, acessories, numberOfPassengers} = req.body;
+    public async createCar(req: Request, res: Response): Promise<Response> {
 
-        console.log(acessories)
-        const carService = new CarService()
-        const car = await carService.createCar({
+        const carService = new CarService();
+        const {model, color, year, valuePerDay, acessories, numberOfPassengers} = req.body;
+        console.log(model, color, year)
+        await carService.createCar({
             model,
             color,
             year,
             valuePerDay,
             acessories,
-            numberOfPassengers,
+            numberOfPassengers
         });
-
         return res.status(201).json(car);
+
     }
 
     public async finAllCars(req: Request, res: Response) {
-        const carService = new CarService()
-
+        const carService = new CarService();
         return res.json(await carService.finAllCars()).status(200);
+
     }
 
     public async updateCar(req: Request, res: Response) {
@@ -41,4 +44,24 @@ export default class CarController {
 
         return res.json(car).status(204)
     }
+
+    public async findById(req: Request, res: Response) {
+        const id = parseInt(req.params.id);
+
+        const carService = new CarService()
+
+        const car = await carService.findById(id);
+
+        return res.json(car).status(200)
+    }
+
+
+    public async deleteCar(req:Request, res:Response){
+        const carService = new CarService();
+        const id = parseInt(req.params.id);
+
+       await carService.delete({id});
+        return res.status(204).send('Movie deleted');
+    }
+
 }
