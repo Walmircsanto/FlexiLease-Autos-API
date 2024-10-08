27c68961336f40879da6f82f9f91aed1ef6c30bc
Walmircsanto <walmirsantosrosa@gmail.com>
2024-10-08 05:25:25 -0300

    :sparkles: feat(CarRoutes) create car routes

---
+++ src/api/modules/cars/routes/CarRoutes.ts
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import { celebrate, Joi, errors, Segments } from "celebrate";
+import { container } from "tsyringe";
+import CarController from "../controller/CarController";
+
+const carRoutes = Router();
+
+const carController = new CarController()
+
+carRoutes.post(
+    "/",
+    celebrate({
+        [Segments.BODY]: Joi.object().keys({
+            model: Joi.string().required(),
+            color: Joi.string().required(),
+            year: Joi.number().required(),
+            valuePerDay: Joi.number().required(),
+            acessories: Joi.array().items(Joi.object({ name: Joi.string().required() })).required(),
+            numberOfPassengers: Joi.number().required(),
+        }),
+    }),
+   carController.createrCar
+);
+
+carRoutes.get("/", carController.finAllCars)
+
+export default carRoutes;
